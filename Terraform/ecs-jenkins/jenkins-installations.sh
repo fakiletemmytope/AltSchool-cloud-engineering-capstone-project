6ac7f7c434f5e6fbfee65a9736d319766d53eda7
Fakile Temitope Oluwaseyi <fakiletemitope@gmail.com>
2024-08-16 11:37:08 +0100

    terraform configure

--- Terraform/ecs-jenkins/jenkins-installations.sh
+++ Terraform/ecs-jenkins/jenkins-installations.sh
@@ -40,6 +40,12 @@ curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo 
 sudo apt update
 sudo apt install caddy -y
 
+#install aws cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+#install kubectl
+sudo apt update
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
